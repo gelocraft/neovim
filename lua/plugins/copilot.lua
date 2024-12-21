7c69c03a9aacae5fecb9e023532b8103ea4b7325
gelocraft <gelocraft@users.noreply.github.com>
2024-12-21 13:52:07 +0000

    nvim(plugin): add github copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,8 @@
+return {
+	'github/copilot.vim',
+	cmd = 'Copilot',
+	keys = {
+		{ '<leader>pe', '<cmd>Copilot enable<cr>', desc = 'Enable Copilot' },
+		{ '<leader>pd', '<cmd>Copilot disable<cr>', desc = 'Disable Copilot' },
+	},
+}

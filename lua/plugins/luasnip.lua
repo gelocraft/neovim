3bbcbbcaec3235c3908d8806e545aea2b4a0e58d
gelocraft <gelocraft@users.noreply.github.com>
2024-12-21 15:23:03 +0000

    nvim(plugin/luasnip): lazy load luasnip

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,10 +1,14 @@
 return {
 	'L3MON4D3/LuaSnip',
+	-- enabled = false,
+	-- lazy = true,
 	keys = function()
-		local luasnip = require 'luasnip'
-		local expand = function() luasnip.expand() end
+		-- local luasnip = require 'luasnip'
+		local expand = function() require('luasnip').expand() end
 		local change_choice = function()
-			if luasnip.choice_active() then luasnip.change_choice(1) end
+			if require('luasnip').choice_active() then
+				require('luasnip').change_choice(1)
+			end
 		end
 		return {
 			{ '<C-space>', mode = 'i', expand },
@@ -12,50 +16,56 @@ return {
 		}
 	end,
 	config = function()
-		local luasnip = require 'luasnip'
+		-- local luasnip = require 'luasnip'
 		local extras = require 'luasnip.extras'
 		local format = require 'luasnip.extras.fmt'
-		luasnip.add_snippets('systemd', require 'snippets.systemd'(luasnip))
-		luasnip.add_snippets(
+		require('luasnip').add_snippets(
+			'systemd',
+			require 'snippets.systemd'(require 'luasnip')
+		)
+		require('luasnip').add_snippets(
 			'all',
-			require 'snippets.all'(luasnip, format, extras)
+			require 'snippets.all'(require 'luasnip', format, extras)
 		)
-		luasnip.add_snippets(
+		require('luasnip').add_snippets(
 			'yaml',
-			require 'snippets.yaml'(luasnip, format, extras)
+			require 'snippets.yaml'(require 'luasnip', format, extras)
 		)
-		luasnip.add_snippets('nginx', require 'snippets.nginx'(luasnip, format))
-		luasnip.add_snippets(
+		require('luasnip').add_snippets(
+			'nginx',
+			require 'snippets.nginx'(require 'luasnip', format)
+		)
+		require('luasnip').add_snippets(
 			'sql',
-			require 'snippets.sql'(luasnip, format, extras)
+			require 'snippets.sql'(require 'luasnip', format, extras)
 		)
-		luasnip.add_snippets(
+		require('luasnip').add_snippets(
 			'lua',
-			require 'snippets.lua'(luasnip, format, extras)
+			require 'snippets.lua'(require 'luasnip', format, extras)
 		)
-		luasnip.add_snippets(
+		require('luasnip').add_snippets(
 			'cs',
-			require 'snippets.csharp'(luasnip, format, extras)
+			require 'snippets.csharp'(require 'luasnip', format, extras)
 		)
-		luasnip.add_snippets(
+		require('luasnip').add_snippets(
 			'python',
-			require 'snippets.python'(luasnip, format, extras)
+			require 'snippets.python'(require 'luasnip', format, extras)
 		)
-		luasnip.add_snippets(
+		require('luasnip').add_snippets(
 			'javascript',
-			require 'snippets.javascript'(luasnip, format)
+			require 'snippets.javascript'(require 'luasnip', format)
 		)
-		luasnip.add_snippets(
+		require('luasnip').add_snippets(
 			'dockerfile',
-			require 'snippets.dockerfile'(luasnip, format)
+			require 'snippets.dockerfile'(require 'luasnip', format)
 		)
-		luasnip.add_snippets(
+		require('luasnip').add_snippets(
 			'editorconfig',
-			require 'snippets.editorconfig'(luasnip, format)
+			require 'snippets.editorconfig'(require 'luasnip', format)
 		)
-		luasnip.add_snippets(
+		require('luasnip').add_snippets(
 			'terraform',
-			require 'snippets.terraform'(luasnip, format, extras)
+			require 'snippets.terraform'(require 'luasnip', format, extras)
 		)
 	end,
 }

cbffadb89d82d7c8ab45cabf609fa493ea9231e6
gelocraft <gelocraft@users.noreply.github.com>
2025-01-25 04:19:28 +0800

    nvim(plugin/telescope): conditionally install telescope fzf extension

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,11 @@ return {
 	'nvim-telescope/telescope.nvim',
 	dependencies = {
 		'nvim-lua/plenary.nvim',
-		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+		{
+			'nvim-telescope/telescope-fzf-native.nvim',
+			build = 'make',
+			cond = function() return vim.fn.executable 'make' == 1 end,
+		},
 	},
 	version = '*',
 	cmd = 'Telescope',

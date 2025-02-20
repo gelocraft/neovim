332d661f0105271351a62f4cb9c606fd0d6a8f9f
gelocraft <gelocraft@users.noreply.github.com>
2025-02-21 02:17:12 +0800

    nvim(config): update the ordering of configs

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,7 +1,7 @@
 require 'config.options'
-require 'config.lazy'
 require 'config.autocmds'
 require 'config.keymaps'
 require 'config.highlights'
 require 'config.diagnostics'
 require 'config.neovide'
+require 'config.lazy'

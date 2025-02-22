8d0536fa0929a186acca6ef1a3660500677b5fbf
gelocraft <gelocraft@users.noreply.github.com>
2025-02-22 05:32:58 +0000

    nvim(config/lsp): update lsp config

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,6 +1,14 @@
 if vim.version.ge(vim.version(), '0.11.0-dev') then
 	vim.lsp.config('*', {
-		root_markers = { '.git', '.github' },
+		root_markers = {
+			'.git',
+			'.github',
+			'.editorconfig',
+		},
 	})
-	vim.lsp.enable({ 'luals', 'gopls' }, vim.g.enable_native_lsp)
+	vim.lsp.enable({
+		'luals',
+		'gopls',
+		'terraformls',
+	}, vim.g.enable_native_lsp)
 end

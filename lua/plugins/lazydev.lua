2d81bc57f2267d38452f0211c6f719557a8f8906
gelocraft <gelocraft@users.noreply.github.com>
2025-02-22 05:11:51 +0000

    nvim(plugin): update lazydev config

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -1,11 +1,8 @@
 return {
 	'folke/lazydev.nvim',
-	ft = 'lua', -- only load on lua files
+	enabled = vim.g.lazydev_enabled,
 	opts = {
-		library = {
-			-- See the configuration section for more details
-			-- Load luvit types when the `vim.uv` word is found
-			{ path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-		},
+		library = { { path = '${3rd}/luv/library', words = { 'vim%.uv' } } },
 	},
+	ft = 'lua', -- only load on lua files
 }

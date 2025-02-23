31f02e0daf9de53614d23937015afff17f3dd09e
gelocraft <gelocraft@users.noreply.github.com>
2025-02-23 01:53:59 +0000

    nvim(lsp): update luals config

--- lsp/luals.lua
+++ lsp/luals.lua
@@ -4,17 +4,18 @@ return {
 	root_markers = { '.stylua.toml', 'stylua.toml' },
 	settings = {
 		Lua = {
-			runtime = {
-				version = 'LuaJIT',
-			},
+			hint = { enable = true },
+			telemetry = { enable = false },
+			runtime = { version = 'LuaJIT' },
 			workspace = {
+				maxPreload = 2000,
+				preloadFileSize = 2000,
 				checkThirdParty = false,
 				library = {
 					vim.env.VIMRUNTIME .. '/lua',
 					'${3rd}/luv/library',
 				},
 			},
-			hint = { enable = true },
 		},
 	},
 }

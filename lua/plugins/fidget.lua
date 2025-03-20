5e09bbdedca5a11dca4d383067db0e7a05346f34
gelocraft <gelocraft@users.noreply.github.com>
2025-03-20 16:07:14 +0000

    nvim(plugin): update fidget config

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -1,13 +1,10 @@
 return {
 	'j-hui/fidget.nvim',
-	tag = 'v1.4.0',
 	event = 'LspAttach',
 	opts = {
 		notification = {
 			window = {
 				border = 'rounded',
-				normal_hl = 'Normal',
-				winblend = 0,
 			},
 		},
 	},

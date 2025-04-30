d459c4061627f0143aaf4c86c6529997ddf34297
gelocraft <gelocraft@users.noreply.github.com>
2025-04-30 12:41:36 +0800

    nvim(lsp): update tsserver settings

--- lsp/tsserver.lua
+++ lsp/tsserver.lua
@@ -6,4 +6,30 @@ return {
 		'javascriptreact',
 		'typescriptreact',
 	},
+	single_file_support = true,
+	settings = {
+		javascript = {
+			inlayHints = {
+				includeInlayEnumMemberValueHints = true,
+				includeInlayFunctionLikeReturnTypeHints = true,
+				includeInlayFunctionParameterTypeHints = true,
+				includeInlayParameterNameHints = 'all', -- 'none' | 'literals' | 'all';
+				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+				includeInlayPropertyDeclarationTypeHints = true,
+				includeInlayVariableTypeHints = false,
+			},
+		},
+
+		typescript = {
+			inlayHints = {
+				includeInlayEnumMemberValueHints = true,
+				includeInlayFunctionLikeReturnTypeHints = true,
+				includeInlayFunctionParameterTypeHints = true,
+				includeInlayParameterNameHints = 'all', -- 'none' | 'literals' | 'all';
+				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+				includeInlayPropertyDeclarationTypeHints = true,
+				includeInlayVariableTypeHints = false,
+			},
+		},
+	},
 }

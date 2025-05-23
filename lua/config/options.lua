01d972103516fcdb567c1d97004ec749bbe5d594
gelocraft <gelocraft@users.noreply.github.com>
2025-05-23 15:31:19 +0800

    nvim(options): update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,11 +3,12 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 vim.g.have_nerd_font = true
+vim.g.file_explorer = 'oil.nvim'
 vim.g.enable_native_lsp = vim.version.ge(vim.version(), '0.11.0-dev')
 vim.g.lazydev_enabled = not vim.g.enable_native_lsp
 vim.g.diagnostic = {
-	virtual_text = { enabled = false },
-	virtual_lines = { enabled = true },
+	virtual_text = { enabled = true },
+	virtual_lines = { enabled = false },
 }
 
 -- no showmode

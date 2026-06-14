0bf67870dd3ea7def89edc0f1d48a23452240738
gelocraft <gelocraft@users.noreply.github.com>
2026-06-15 03:13:03 +0800

    chore(lsp): update rust_analyzer config

--- lsp/rust_analyzer.lua
+++ lsp/rust_analyzer.lua
@@ -2,4 +2,17 @@ return {
 	cmd = { 'rust-analyzer' },
 	filetypes = { 'rust' },
 	single_file_support = true,
+	settings = {
+		['rust-analyzer'] = {
+			imports = {
+				granularity = {
+					group = 'module',
+				},
+				prefix = 'self',
+			},
+			completion = {
+				autoimport = true,
+			},
+		},
+	},
 }

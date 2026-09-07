002713d50a4f1422f73b485742d971706a6c8d39
gelocraft <gelocraft@users.noreply.github.com>
2026-09-07 09:22:05 +0800

    chore(completion/blink): update config

--- lua/plugins/completion/blink.lua
+++ lua/plugins/completion/blink.lua
@@ -10,7 +10,6 @@ return {
 	---@module 'blink.cmp'
 	---@type blink.cmp.Config
 	opts = {
-		signature = { enabled = true, window = { show_documentation = false } },
 
 		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
 		keymap = {
@@ -44,11 +43,48 @@ return {
 			nerd_font_variant = 'mono',
 		},
 
+		signature = {
+			enabled = true,
+			window = {
+				border = 'single',
+				show_documentation = false,
+				treesitter_highlighting = true,
+			},
+		},
+
 		-- (Default) Only show the documentation popup when manually triggered
 		completion = {
-			menu = { auto_show = false },
-			documentation = { auto_show = false, auto_show_delay_ms = 500 },
+			list = { max_items = 10 }, -- Maximum number of items to display
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 50,
+				update_delay_ms = 50,
+				treesitter_highlighting = true,
+				window = {
+					border = 'single',
+				},
+			},
 			ghost_text = { enabled = true, show_with_menu = false },
+			menu = {
+				auto_show = true,
+				max_height = 5,
+				border = nil,
+				draw = {
+					padding = { 1, 1 },
+					treesitter = { 'lsp' },
+					components = {
+						label = {
+							ellipsis = true,
+							width = { fill = true, max = 20 },
+						},
+					},
+					columns = {
+						{ 'source_name' },
+						{ 'label', 'label_description', gap = 1 },
+						{ 'kind_icon', 'kind', gap = 1 },
+					},
+				},
+			},
 		},
 
 		-- Default list of enabled providers defined so that you can extend it

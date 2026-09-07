5370d09662d2477429ac8cfc874ad8d1f93d626a
gelocraft <gelocraft@users.noreply.github.com>
2026-09-07 09:29:24 +0800

    feat(todo-comments): add keymaps for todos

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,6 +1,10 @@
 return {
 	'folke/todo-comments.nvim',
 	dependencies = { 'nvim-lua/plenary.nvim' },
+	keys = {
+		{ '<leader>to', '<cmd>TodoTelescope<cr>', desc = ':TodoTelescope' },
+		{ '<leader>do', '<cmd>TodoTrouble<cr>', desc = ':TodoTrouble' },
+	},
 	cmd = { 'TodoTelescope', 'TodoTrouble' },
 	opts = {},
 }
